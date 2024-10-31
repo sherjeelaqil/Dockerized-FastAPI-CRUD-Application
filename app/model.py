f0c2ac0655d588be494408acdd2f467fdb72ea1b
sherjeel <sherjeelm8@gmail.com>
2024-10-31 06:46:11 +0500

    added model file

---
+++ app/model.py
@@ -0,0 +1,17 @@
+# app/models.py
+from sqlalchemy import Column, Integer, String, Enum
+from app.database import Base
+import enum
+
+class UserRole(str, enum.Enum):
+    admin = "admin"
+    user = "user"
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+    role = Column(Enum(UserRole))
